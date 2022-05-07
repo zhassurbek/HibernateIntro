1f13c6c3700613784d4537e9d2a44722d3d991e0
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2022-05-07 12:56:24 +0600

    Hibernate Project Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+//import entity.User;
+import jakarta.persistence.Query;
+import org.hibernate.Session;
+
+import jakarta.persistence.Query;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.getTransaction().begin();
+
+        session.close();
+        HibernateUtil.close();
+
+        //save new data
+        session.getTransaction().begin();
+//
+//        User user = new User();
+//        user.setEmail("asdf2@gmail.com");
+//        user.setUsername("makhanov2");
+//        user.setPassword("asdfasdf");
+//        // id не указываем
+//
+//        session.save(user);
+//
+//        session.getTransaction().commit();
+//        session.close();
+//        HibernateUtil.close();
+
+        //HQL
+//        Query query = session.createQuery("From User");
+//        List<User> users = query.getResultList();
+//
+//        System.out.println(users.get(0));
+//        session.close();
+//        HibernateUtil.close();
+
     }
+
 }
